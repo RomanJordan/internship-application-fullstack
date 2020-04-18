77f35c92980c88c86dd7090f3e995f7c25b4d27b
Jordan Roman <32529400+RomanJordan@users.noreply.github.com>
2020-04-17 20:43:15 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const COOKIE_NAME = '__uid'
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -5,8 +6,90 @@ addEventListener('fetch', event => {
  * Respond with hello worker text
  * @param {Request} request
  */
+function getCookie(request, name) {
+  let result = null
+  let cookieString = request.headers.get('Cookie')
+  if (cookieString) {
+    let cookies = cookieString.split(';')
+    cookies.forEach(cookie => {
+      let cookieName = cookie.split('=')[0].trim()
+      if (cookieName === name) {
+        let cookieVal = cookie.split('=')[1]
+        result = cookieVal
+      }
+    })
+  }
+  console.log(result)
+  return result
+}
+
+
+function getRandomURL(items){
+  var item = items[Math.floor(Math.random() < 0.5)];
+  // console.log(item);
+  return item;
+}
+
+
+class TitleHandler {
+  element(element) {
+    element.setInnerContent("Jordan's take home project")
+  }
+}
+
+class BodyHandler {
+  element(element) {
+    element.append("\nThe power of Cloudflare workers!")
+  }
+}
+
+class URLHandler {
+  element(element) {
+    let mysite = 'https:/romanjordan.github.io/JRoman/'
+    element.setInnerContent("Visit my site & my Github!")
+    // console.log(element.getAttribute())
+    element.setAttribute("href", mysite)
+    // console.log(element.getAttribute("class"))
+  }
+}
+
+class h1TitleHandler {
+  element(element) {
+    element.append(" provided by the Cloudflare API!")
+  }
+}
+
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  
+  var urlVariants = []
+  var url = 'https://cfw-takehome.developers.workers.dev/api/variants';
+  
+  var res = await fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) =>{
+      urlVariants.push(data.variants[0])
+      urlVariants.push(data.variants[1])
+    });
+  
+  
+  var randNumb = Math.floor(Math.random() * 2)
+  
+  var randresp = await fetch(urlVariants[randNumb])
+  
+  
+  const cookie = getCookie(request, COOKIE_NAME)
+  
+  var rewrite = new HTMLRewriter()
+    .on("title", new TitleHandler())
+    .on("a#url", new URLHandler())
+    .on("p#description", new BodyHandler())
+    .on("h1#title", new h1TitleHandler())
+    .transform(randresp)
+    
+
+  
+  return (rewrite)
 }
